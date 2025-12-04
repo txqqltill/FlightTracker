8d06917bc3c9bc5729574590479ebe0bed595e27
Till Kleditzsch <till.kleditzsch@alumni.fh-aachen.de>
2025-12-04 21:14:17 +0100

    Update FlightTracker.cpp

--- FlightTracker.cpp
+++ FlightTracker.cpp
@@ -26,54 +26,79 @@ int main(){
 
     Drawer drawer(lcd);
 
-    uint8_t start_index = 1;
+    uint8_t index = 1;
     
     Joystick joy;
 
     Button s1(EDU_BUTTON1);
     Button s2(EDU_BUTTON2);
     bool inSubMenu = false;
+    uint8_t subPageCounter = 0;
     API api;
     List<Flight> flights = api.getTopFlights();
-    drawer.drawTable(flights, start_index);
+    drawer.drawTable(flights, index);
 
     while (1)
     {
-        bool down = joy.yMovedDown();
-        bool up = joy.yMovedUp();
+        bool right = joy.MovedRight();
+        bool left = joy.MovedLeft();
+        bool down = joy.MovedDown();
+        bool up = joy.MovedUp();
         bool draw_needed = false;
-        
-        if (s1.pressed()){
+
+        if (s1.pressed() && subPageCounter == 0){
+            logInfo("S1");
             inSubMenu = true;
-            Flight flight = flights.get(start_index - 1);
+            subPageCounter = 1;
+            Flight flight = flights.get(index - 1);
             auto id = flight.flightId;
             SpecificFlightData data = api.getSpecificFlightData(flight.flightId);
-            drawer.drawSpecifigFlightInformation(data);
+            drawer.initSubPage(std::move(data));
         }
 
-        if (s2.pressed()){
+        if (s2.pressed() && subPageCounter != 0){
+            logInfo("S2");
             inSubMenu = false;
             draw_needed = true;
+            subPageCounter = 0;
         }
 
         if (down && inSubMenu == false) {
-            if (start_index < DISPLAY_LIMIT) {
-                start_index++;
+            if (index < DISPLAY_LIMIT) {
+                index++;
                 draw_needed = true;
-                logNumber(start_index);
+                logNumber(index);
             }
+        }
+
+        if (right && subPageCounter >= 1){
+            subPageCounter++;
+            if (subPageCounter > MAXSUBPAGE) 
+                subPageCounter = MAXSUBPAGE;
+
+            logNumber(subPageCounter);
+            drawer.drawSubPage(subPageCounter);
+        }
+
+        if (left && subPageCounter >= 1){
+            subPageCounter--;
+            if (subPageCounter <= 0)
+                subPageCounter = 1;
+            
+            logNumber(subPageCounter);
+            drawer.drawSubPage(subPageCounter);
         }
         
         if (up && inSubMenu == false) {
-            if (start_index > 1) {
-                start_index--;
+            if (index > 1) {
+                index--;
                 draw_needed = true;
-                logNumber(start_index);
+                logNumber(index);
             }
         }
 
         if (draw_needed) {
-            drawer.drawTable(flights, start_index);
+            drawer.drawTable(flights, index);
         }
     }
 }
